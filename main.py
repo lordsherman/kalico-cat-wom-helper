2baea5b3a269f50aa5ddffcb54a0efc503ccad39
lordsherman <kyleeverling5244@gmail.com>
2022-08-12 05:44:08 -0400

    add competition detail gathering

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import requests
 import json
 
-def main():
-    metric = input("What is this weeks skill?")
+def gather_details():
+    title_numb = input("What number SOTW is running this week? ")
+    date = input("What day will the competition start? (MM-DD-YYYY) ")
+    usable_metrics = open("usable-metrics.txt", "r")
+    metric_list = [line.strip() for line in usable_metrics]
+    usable_metrics.close()
+    
+    valid_metric = True
+    while valid_metric:
+        metric = input("What is this weeks skill? (lowercase skill name) ")
+        if metric not in metric_list:
+            print("Please enter a usable metric. Skills need to be all lowercase.")
+        else:
+            valid_metric = False
+    
+    return title_numb, metric, date
 
+def main():
+    gather_details()
+    
 if __name__ == '__main__':
     main()
 
