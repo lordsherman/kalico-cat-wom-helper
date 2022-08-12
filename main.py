3eb8ff7cb34bccce5c0676d45b8172617d76e003
lordsherman <kyleeverling5244@gmail.com>
2022-08-12 05:14:41 -0400

    remove uneeded group pull

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import requests
 import json
 
-def get_members():
-    id_response = requests.get("https://api.wiseoldman.net/groups/909/members")
-    return id_response
-
 def main():
-    members = get_members()
-    print(members.json())
+    metric = input("What is this weeks skill?")
 
 if __name__ == '__main__':
     main()
+
+# Example POST Payload
+# {
+#   "title": "Fishing SOTW", 
+#   "metric": "fishing", 
+#   "startsAt": "2020-12-21T19:00:00.000Z", 
+#   "endsAt": "2020-12-27T19:00:00.000Z", 
+#   "groupId": "909", 
+#   "groupVerificationCode": "ENV"
+# }
+
