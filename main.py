360c70aad7eab699a32ba8b99975b0c2772d0227
lordsherman <kyleeverling5244@gmail.com>
2023-01-11 21:45:37 -0500

    update example payload

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 #   "metric": "fishing", 
 #   "startsAt": "2020-12-21T19:00:00.000Z", 
 #   "endsAt": "2020-12-27T19:00:00.000Z", 
-#   "groupId": "909", 
+#   "groupId": 909, 
 #   "groupVerificationCode": "ENV"
 # }
 
